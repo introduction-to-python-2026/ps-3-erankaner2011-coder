fc84beefbe6ce2eb144652cf8c3a157da0959cea
erankaner2011-coder <erankaner2011@gmail.com>
2025-11-09 13:51:42 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,2 +1,7 @@
 def approximate_pi(n_terms):
-    pass # replace pass with your code
+  leibniz_series = []
+  for i in range(n_terms):
+      sign = (-1)**i/(2*i+1)
+      leibniz_series.append(sign)
+  return (sum(leibniz_series)*4)
+
